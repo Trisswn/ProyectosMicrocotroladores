bb9b52de49404e160c4855d77ce4326778754a36
Trisswn <2409226@upy.edu.mx>
2026-02-16 21:22:27 -0600

    Practica2Unidad2

---
+++ PRACTICA2U2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PRACTICA2U2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PRACTICA2U2.X.production.hex
